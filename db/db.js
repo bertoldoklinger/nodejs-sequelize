00f00da422cfa029bc25bea8aa3476449f572368
bertoldoklinger <bertoldokbrj@gmail.com>
2023-02-27 01:40:55 -0300

    refactor: deleted try catch with sequelize connection

--- db/db.js
+++ db/db.js
@@ -5,11 +5,4 @@ const sequelize = new Sequelize('nodesequelize', 'root', '', {
   dialect: 'mysql'
 })
 
-// try {
-//   sequelize.authenticate();
-//   console.log('Connection has been established successfully.');
-// } catch (err) {
-//   console.log('Não foi possível conectar: ', err)
-// }
-
 module.exports = sequelize;
